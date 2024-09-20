7bef0ce589aa1a968a27624093c194a978fcf031
keertimani <keerthiga.ramesh.m@gmail.com>
2024-09-20 10:39:49 +0530

    Update PalindromeOrNot.java

--- day2/PalindromeOrNot.java
+++ day2/PalindromeOrNot.java
@@ -4,8 +4,27 @@ public class PalindromeOrNot {
 
 	public static void main(String[] args) {
 
+int num = 1234;
+		int temp = num;
+		int rev= 0;
 		
+		while(num!=0)
+		{
+			rev= rev*10+ num%10;
+			num= num/10;
+			
+		}
 		
+		if (temp==rev) {
+			System.out.println("number is palindrome:" +temp);
+		}
+		else {
+			System.out.println(" number is not palindrome:" +temp);
+		}
 	}
 
 }
+		
+	
+
+
